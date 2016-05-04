6431b62b4ce9adbabd98a7ab5eb85a1a4d88ffc7
Michael Arbogast <marboga@gmail.com>
2016-05-03 17:45:11 -0700

    Ninja Gold implemented for iOS.

--- Ninja_Gold/ViewController.swift
+++ Ninja_Gold/ViewController.swift
@@ -9,17 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var buildingLabelsCollection: [UILabel]!
+    
+    var score = 0
+    var changeInScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        hideLabels()
+        resetScore()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buildingButtonPressed(sender: UIButton) {
+        print(sender.tag)
+        hideLabels()
+        if sender.tag == 1 {
+            changeInScore = Int(arc4random_uniform(10)) + 10
+            score += changeInScore
+            print(changeInScore, score)
+        }
+        else if sender.tag == 2 {
+            changeInScore = Int(arc4random_uniform(5)) + 5
+            score += changeInScore
+            print(changeInScore, score)
+        }
+        else if sender.tag == 3 {
+            changeInScore = Int(arc4random_uniform(3)) + 2
+            score += changeInScore
+            print(changeInScore, score)
+        }
+        else if sender.tag == 4 {
+            if Int(arc4random_uniform(100)) > 50 {
+                changeInScore = Int(arc4random_uniform(50))
+                score += changeInScore
+                print(changeInScore, score)
+            }
+            else {
+                changeInScore = -Int(arc4random_uniform(50))
+                score += changeInScore
+                print(changeInScore, score)
+            }
+        }
+        updateScore(sender)
+    }
+    
+    func updateScore(sender: UIButton) {
+        if score < 0 {
+            scoreLabel.text = "You have lost."
+            hideLabels()
+        }
+        else{
+            scoreLabel.text = "Score: \(self.score)"
+            buildingLabelsCollection[sender.tag - 1].hidden = false
+            buildingLabelsCollection[sender.tag - 1].text = "In reward for your efforts, you have been granted \(changeInScore) gold."
+            
 
+        }
+    }
 
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        resetScore()
+    }
+    
+    func resetScore() {
+        score = 0
+        scoreLabel.text = "Score: \(self.score)"
+        hideLabels()
+    }
+    
+    func hideLabels() {
+        for eachLabel in buildingLabelsCollection {
+            eachLabel.hidden = true
+        }
+    }
+    
 }
 
